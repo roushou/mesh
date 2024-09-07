4e19bccffe9a948fa3ef4c0925d141ea2e3f382e
roushou <roushou9@gmail.com>
2024-09-08 02:29:49 +0700

    chore: update prediction types

--- examples/replicate-basic/src/main.rs
+++ examples/replicate-basic/src/main.rs
@@ -1,10 +1,23 @@
-use replic::{client::Client, config::Config};
+use replic::{
+    client::{Client, CreatePrediction},
+    config::Config,
+};
 
 #[tokio::main]
 async fn main() {
     let config = Config::from_env().unwrap();
     let client = Client::new(config).unwrap();
 
-    let collections = client.collections().await.unwrap();
+    let collections = client
+        .create_prediction(CreatePrediction {
+            version: "f2ab8a5bfe79f02f0789a146cf5e73d2a4ff2684a98c2b303d1e1ff3814271db".to_string(),
+            input: serde_json::json!({
+                "prompt": "black forest gateau cake spelling out the words \"FLUX SCHNELL\", tasty, food photography, dynamic shot"
+            }),
+            webhook: None,
+            webhook_event_filters: None,
+        })
+        .await
+        .unwrap();
     println!("{:?}", collections);
 }
